98acf0150c289890feee9fbf6eacf9f1447daf25
SnehalG148 <snehal.rodi@waiin.com>
2025-02-13 05:24:51 +0000

    implemented a few scientific functions

--- src/script.js
+++ src/script.js
@@ -13,7 +13,19 @@ document.addEventListener("DOMContentLoaded", function(){
         const convertedValue = currentValue
         .replace("×", "*")
         .replace("÷", "/")
-        .replace("%", "*0.01");
+        .replace("%", "*0.01")
+        .replace("sin","Math.sin")
+        .replace("cos","Math.cos")
+        .replace("ln","Math.log")
+        .replace("π","Math.PI")
+        .replace("log","Math.log10")
+        .replace("e","Math.E")
+        .replace("tan","Math.tan")
+        .replace("√","Math.sqrt");
+
+        
+        
+
 
 
         console.log('convertedValue:',convertedValue)
@@ -29,18 +41,25 @@ document.addEventListener("DOMContentLoaded", function(){
       // console.log('button clicked:',button.innerText);
       const value = button.innerText;
      
-      
-
+      try{
         if(value =="AC"){
-          currentValue = "";
-          display.value = currentValue;
-        }else if(value == "="){
-          evaluateResult();
-        }else{
-          currentValue += value;
-          // console.log('currentValue:',currentValue);
-          display.value = currentValue;
-        }                           
+            currentValue = "";
+            display.value = currentValue;
+          }else if(value == "="){
+            evaluateResult();
+          }else{
+            currentValue += value;
+            // console.log('currentValue:',currentValue);
+            display.value = currentValue;
+          }       
+      }catch(error){
+        console.error(error);
+        currentValue = "ERROR";
+        display.value = currentValue;
+
+      }
+
+                            
       })
     }  
   
